18788ecc2cab0e1d0e81a5ff0bf539ba9dd172dd
AnamikaUnnikrishnan <127471785+AnamikaUnnikrishnan@users.noreply.github.com>
2023-04-30 22:40:28 +0530

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -3,7 +3,7 @@ from frame import setup as su
 
 
 st.header('NB Text Classifier')
-text = st.text_input('Enter your text:')
+input = st.text_area('Enter your text:', value="")
 if st.button("Predict"):
-    vec = su.vector.transform([text]).toarray()
+    vec = su.vector.transform([input]).toarray()
     st.write('Label:',str(list(su.naivebayes.predict(vec))[0]).replace('0', 'TECH').replace('1', 'BUSINESS').replace('2', 'SPORTS').replace('3','ENTERTAINMENT').replace('4','POLITICS'))
